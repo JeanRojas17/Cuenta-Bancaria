a0e62b80b2a507f0747da818f816898c15bb8fa5
JeanRojas17 <jeanpaulrojas17@gmail.com>
2026-03-08 20:51:45 -0500

    Actualizando método main.

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,6 +2,16 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CuentaAhorros ca = new CuentaAhorros(50000, 12);
+
+        ca.consignar(20000);
+        ca.consignar(10000);
+        ca.retirar(15000);
+        ca.retirar(23000);
+        ca.retirar(22000);
+        ca.retirar(7000);
+        ca.retirar(1000);
+        ca.extractoMensual();
+        ca.imprimir();
     }
 }
