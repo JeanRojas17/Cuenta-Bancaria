5f5e01094406ec4d5fe67eb60daf4569a3093412
JeanRojas17 <jeanpaulrojas17@gmail.com>
2026-03-08 19:54:09 -0500

    Agregando la clase CuentaAhorros.

---
+++ src/main/java/com/example/CuentaAhorros.java
@@ -0,0 +1,46 @@
+package com.example;
+
+public class CuentaAhorros extends Cuenta {
+
+    protected boolean activa;
+
+    public CuentaAhorros(float saldo, float tasa) {
+        super(saldo, tasa);
+        activa = saldo >= 10000;
+    }
+
+    @Override
+    public void consignar(float cantidad) {
+        if(activa) {
+            super.consignar(cantidad);
+        } else {
+            System.out.println("Cuenta inactiva. No se pueden realizar consignaciones.");
+        }
+    }
+
+    @Override
+    public void retirar(float cantidad) {
+        if(activa) {
+            super.retirar(cantidad);
+        } else {
+            System.out.println("Cuenta inactiva. No se pueden realizar retiros.");
+        }
+    }
+
+    public void extractoMensual() {
+        if(numeroRetiros > 4) {
+            comisionMensual = (numeroRetiros - 4) * 1000;
+        }
+
+        super.extractoMensual();
+        activa = saldo >= 10000;
+    }
+
+    public void imprimir() {
+        System.out.println("* * * *   Cuenta de Ahorros   * * * *");
+        System.out.println("Saldo: $" +saldo);
+        System.out.println("Comision mensual: $" +comisionMensual);
+        System.out.println("Número de transacciones: " +(numeroConsignaciones + numeroRetiros));
+        System.out.println("Cuenta activa: " +(activa ? "Sí" : "No"));
+    }
+}
